4500f117e4f5e7cbd2c864300b0eda4e59f4e01a
Kareem Adel <adel30320@gmail.com>
2023-09-21 15:05:18 +0300

    MassageCollection and add Mothod ShowEmptyDataMassage()

--- GolbalUsing.cs
+++ GolbalUsing.cs
@@ -1,8 +1,5 @@
 ﻿global using System;
-global using System.Collections.Generic;
 global using System.Linq;
-global using System.Text;
-global using System.Threading.Tasks;
 global using ASRFLY.Code;
 global using ASRFLY.Gui.GuiHome;
 global using ASRFLY.Properties;

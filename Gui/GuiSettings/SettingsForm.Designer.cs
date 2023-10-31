95bccfa56a59f61c3034f8a0ab4748e7c52d5305
Kareem Adel <adel30320@gmail.com>
2023-10-31 02:34:22 +0200

    تصميم نافذة الاعدادات وحفظها

--- Gui/GuiSettings/SettingsForm.Designer.cs
+++ Gui/GuiSettings/SettingsForm.Designer.cs
@@ -28,16 +28,390 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
+            groupBox1 = new GroupBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            numericUpDown2 = new NumericUpDown();
+            label4 = new Label();
+            pictureBox1 = new PictureBox();
+            linkLabel1 = new LinkLabel();
+            buttonSave = new Button();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
+            button1 = new Button();
+            button2 = new Button();
+            groupBox4 = new GroupBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            label6 = new Label();
+            numericUpDown3 = new NumericUpDown();
+            label7 = new Label();
+            textBox3 = new TextBox();
+            label8 = new Label();
+            textBox4 = new TextBox();
+            button3 = new Button();
+            label9 = new Label();
+            textBox5 = new TextBox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
             SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(buttonSave);
+            groupBox1.Controls.Add(linkLabel1);
+            groupBox1.Controls.Add(pictureBox1);
+            groupBox1.Controls.Add(numericUpDown2);
+            groupBox1.Controls.Add(numericUpDown1);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(459, 558);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "الاعدادات العامة";
+            // 
+            // textBox1
+            // 
+            textBox1.Cursor = Cursors.IBeam;
+            textBox1.Location = new Point(17, 72);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(319, 32);
+            textBox1.TabIndex = 1;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(430, 80);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 24);
+            label1.TabIndex = 0;
+            label1.Text = "السيرفر";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(243, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(194, 24);
+            label2.TabIndex = 0;
+            label2.Text = "فترة عرض الاشعارات(ثوانى) ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(271, 169);
+            label3.Name = "label3";
+            label3.Size = new Size(166, 24);
+            label3.TabIndex = 0;
+            label3.Text = "عدد البيانات المعروضة";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(15, 100);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(163, 32);
+            numericUpDown1.TabIndex = 2;
+            numericUpDown1.Value = new decimal(new int[] { 2, 0, 0, 0 });
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(15, 161);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(163, 32);
+            numericUpDown2.TabIndex = 2;
+            numericUpDown2.Value = new decimal(new int[] { 25, 0, 0, 0 });
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(148, 244);
+            label4.Name = "label4";
+            label4.Size = new Size(123, 28);
+            label4.TabIndex = 0;
+            label4.Text = "شعار المؤسسة";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(116, 293);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(170, 112);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(139, 420);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(95, 24);
+            linkLabel1.TabIndex = 4;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "تحميل صورة";
+            // 
+            // buttonSave
+            // 
+            buttonSave.BackColor = SystemColors.ActiveCaption;
+            buttonSave.FlatAppearance.BorderSize = 0;
+            buttonSave.Font = new Font("Calibri", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonSave.ForeColor = Color.Black;
+            buttonSave.Image = Resources.Save_1;
+            buttonSave.ImageAlign = ContentAlignment.MiddleLeft;
+            buttonSave.Location = new Point(15, 482);
+            buttonSave.Margin = new Padding(5);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Padding = new Padding(5);
+            buttonSave.Size = new Size(422, 55);
+            buttonSave.TabIndex = 7;
+            buttonSave.Text = "حفظ";
+            buttonSave.UseVisualStyleBackColor = false;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(groupBox4);
+            groupBox2.Controls.Add(groupBox3);
+            groupBox2.Font = new Font("Calibri", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            groupBox2.Location = new Point(477, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(524, 571);
+            groupBox2.TabIndex = 0;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "قواعد البيانات";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(button2);
+            groupBox3.Controls.Add(button1);
+            groupBox3.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            groupBox3.Location = new Point(19, 441);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(488, 117);
+            groupBox3.TabIndex = 0;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "النسخ الاحتياطى والتعيين";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.White;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.Black;
+            button1.Image = Resources.Save_1;
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(276, 42);
+            button1.Margin = new Padding(5);
+            button1.Name = "button1";
+            button1.Padding = new Padding(5);
+            button1.Size = new Size(204, 55);
+            button1.TabIndex = 7;
+            button1.Text = "النسخ الاحتياطى";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.White;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = Color.Black;
+            button2.Image = Resources.Save_1;
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(8, 42);
+            button2.Margin = new Padding(5);
+            button2.Name = "button2";
+            button2.Padding = new Padding(5);
+            button2.Size = new Size(258, 55);
+            button2.TabIndex = 7;
+            button2.Text = "     أستعادة النسخة الاحتياطية\r\n";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(button3);
+            groupBox4.Controls.Add(radioButton2);
+            groupBox4.Controls.Add(radioButton1);
+            groupBox4.Controls.Add(textBox2);
+            groupBox4.Controls.Add(label5);
+            groupBox4.Controls.Add(numericUpDown3);
+            groupBox4.Controls.Add(textBox4);
+            groupBox4.Controls.Add(label8);
+            groupBox4.Controls.Add(textBox3);
+            groupBox4.Controls.Add(label7);
+            groupBox4.Controls.Add(textBox1);
+            groupBox4.Controls.Add(label1);
+            groupBox4.Controls.Add(label6);
+            groupBox4.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            groupBox4.Location = new Point(10, 31);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(497, 404);
+            groupBox4.TabIndex = 1;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "نص الأتصال";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Checked = true;
+            radioButton1.Location = new Point(145, 31);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(67, 28);
+            radioButton1.TabIndex = 0;
+            radioButton1.Text = "محلى";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(17, 31);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(66, 28);
+            radioButton2.TabIndex = 0;
+            radioButton2.Text = "شبكى";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(385, 133);
+            label5.Name = "label5";
+            label5.Size = new Size(106, 24);
+            label5.TabIndex = 0;
+            label5.Text = "قاعدة البيانات";
+            // 
+            // textBox2
+            // 
+            textBox2.Cursor = Cursors.IBeam;
+            textBox2.Location = new Point(17, 125);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(319, 32);
+            textBox2.TabIndex = 1;
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(351, 186);
+            label6.Name = "label6";
+            label6.Size = new Size(138, 24);
+            label6.TabIndex = 0;
+            label6.Text = "فترة الاتصال(ثانية) ";
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Location = new Point(17, 178);
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(319, 32);
+            numericUpDown3.TabIndex = 2;
+            numericUpDown3.Value = new decimal(new int[] { 2, 0, 0, 0 });
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(377, 239);
+            label7.Name = "label7";
+            label7.Size = new Size(112, 24);
+            label7.TabIndex = 0;
+            label7.Text = "اسم المستخدم";
+            // 
+            // textBox3
+            // 
+            textBox3.Cursor = Cursors.IBeam;
+            textBox3.Location = new Point(17, 231);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(319, 32);
+            textBox3.TabIndex = 1;
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(414, 292);
+            label8.Name = "label8";
+            label8.Size = new Size(75, 24);
+            label8.TabIndex = 0;
+            label8.Text = "كلمة السر";
+            // 
+            // textBox4
+            // 
+            textBox4.Cursor = Cursors.IBeam;
+            textBox4.Location = new Point(17, 284);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(319, 32);
+            textBox4.TabIndex = 1;
+            textBox4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveCaption;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.Font = new Font("Calibri", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.ForeColor = Color.Black;
+            button3.Image = Resources.Save_1;
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(17, 337);
+            button3.Margin = new Padding(5);
+            button3.Name = "button3";
+            button3.Padding = new Padding(5);
+            button3.Size = new Size(432, 59);
+            button3.TabIndex = 7;
+            button3.Text = "حفظ";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(333, 55);
+            label9.Name = "label9";
+            label9.Size = new Size(104, 24);
+            label9.TabIndex = 0;
+            label9.Text = "اسم المؤسسة";
+            // 
+            // textBox5
+            // 
+            textBox5.Cursor = Cursors.IBeam;
+            textBox5.Location = new Point(15, 47);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(285, 32);
+            textBox5.TabIndex = 1;
+            textBox5.TextAlign = HorizontalAlignment.Center;
             // 
             // SettingsForm
             // 
             AutoScaleDimensions = new SizeF(10F, 24F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1008, 701);
+            ClientSize = new Size(1013, 587);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
             Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
             FormBorderStyle = FormBorderStyle.FixedDialog;
-            Margin = new Padding(4, 4, 4, 4);
+            Margin = new Padding(4);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "SettingsForm";
@@ -45,9 +419,49 @@
             RightToLeftLayout = true;
             StartPosition = FormStartPosition.CenterScreen;
             Text = " أعدادات النظام";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox2.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numericUpDown1;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox1;
+        private Label label4;
+        private Button buttonSave;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private Button button1;
+        private Button button2;
+        private GroupBox groupBox4;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private TextBox textBox2;
+        private Label label5;
+        private NumericUpDown numericUpDown3;
+        private Label label6;
+        private Button button3;
+        private TextBox textBox4;
+        private Label label8;
+        private TextBox textBox3;
+        private Label label7;
+        private TextBox textBox5;
+        private Label label9;
     }
 }
